7030443f58464d71af8f79f8670f0085f6382f65
Christopher <leechrs1@gmail.com>
2022-04-13 18:35:58 -0500

    able to console.log weatherApi...still don t know how to work with the data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,12 @@ var weatherApi = function() {
     var latitude = "44.986656";
     var longitude = "-93.258133";
     var apiKey = "5ba108ef8304fea45cf37033129cadbd";
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`);
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+    .then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        })
+    })
 }
 
 weatherApi();
