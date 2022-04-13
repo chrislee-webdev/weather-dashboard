04e98c7efc9630f36247f9e68053b5f5bff87f58
Christopher <leechrs1@gmail.com>
2022-04-13 17:34:59 -0500

    created weatherApi function...now to figure out how to use the dat a it returns

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Request for OpenWeatherAPI
+var weatherApi = function() {
+    var latitude = "44.986656";
+    var longitude = "-93.258133";
+    var apiKey = "5ba108ef8304fea45cf37033129cadbd";
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`);
+}
+
+weatherApi();
