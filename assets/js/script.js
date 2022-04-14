efde85bd928af89fb0cd49cc6d4eb9576a15a570
Christopher <leechrs1@gmail.com>
2022-04-13 21:24:35 -0500

    css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 //Request for OpenWeatherAPI
-var weatherApi = function() {
-    var latitude = "44.986656";
-    var longitude = "-93.258133";
-    var apiKey = "5ba108ef8304fea45cf37033129cadbd";
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+// var weatherApi = function() {
+//     var latitude = "44.986656";
+//     var longitude = "-93.258133";
+//     var apiKey = "5ba108ef8304fea45cf37033129cadbd";
+//     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+//     .then(function(response){
+//         response.json().then(function(data){
+//             console.log(data);
+//         })
+//     })
+// }
+
+// weatherApi();
+
+var latitude = "44.986656";
+var longitude = "-93.258133";
+var apiKey = "5ba108ef8304fea45cf37033129cadbd";
+
+fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
     .then(function(response){
         response.json().then(function(data){
             console.log(data);
+            // document.getElementById('currentTemp').innerHTML = data.current.temp;
         })
     })
-}
-
-weatherApi();
