890d4449ee00e4a24fc5258f5e3a05c2da254088
Christopher <leechrs1@gmail.com>
2022-04-16 19:31:54 -0500

    Search function works...need to ficure out how to add history of buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-var latitude = "44.986656";
-var longitude = "-93.258133";
 var apiKey = "5ba108ef8304fea45cf37033129cadbd";
 var geoKey = "9eca3ef09bf33b75f86893b2ec7e832d";
 var currentDate = document.getElementById("currentDate");
@@ -9,13 +7,11 @@ var day2 = moment().add(2, 'day').format("MMMM Do");
 var day3 = moment().add(3, 'day').format("MMMM Do");
 var day4 = moment().add(4, 'day').format("MMMM Do");
 var day5 = moment().add(5, 'day').format("MMMM Do");
+
 //Submit button click event and append new search button to list
 var searchHistory = document.getElementById('searchHistory')
 var search = document.getElementById('search');
 var submit = document.getElementById('submit');
-
-console.log(day);
-console.log(tomorrow);
 
 //Display date
 currentDate.innerHTML = day;
@@ -27,15 +23,80 @@ document.getElementById('day-5').innerHTML = day5;
 
 //Geocoding location
 var citySearch = submit.addEventListener('click', function(){
-    console.log(search.value);
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${citySearch}&limit=5&appid=${geoKey}`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${search.value}&limit=5&appid=${geoKey}`)
     .then(function(response){
         response.json().then(function(data){
             console.log(data);
-        })
-    })
-})
+            var latitude = data[0].lat;
+            var longitude = data[0].lon;
 
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+    .then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+            //Add name of city to HTML
+            document.getElementById('city').innerHTML = search.value + " - " + day;
+            //Get weather icon
+            var weatherIcon = data.current.weather[0].icon;
+            console.log(weatherIcon)
+            var weatherIconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
+            document.getElementById('weatherId').src = weatherIconUrl;
+
+            document.getElementById('currentTemp').innerHTML = data.current.temp + " °F";
+            document.getElementById('currentWind').innerHTML = data.current.wind_speed + " MPH";
+            document.getElementById('currentHumidity').innerHTML = data.current.humidity + " %";
+            document.getElementById('currentUv').innerHTML = data.current.uvi;
+            //Next Day Info
+            var weatherIcon1 = data.daily[0].weather[0].icon;
+            console.log(weatherIcon1)
+            var weatherIcon1Url = "http://openweathermap.org/img/wn/" + weatherIcon1 + "@2x.png"
+            document.getElementById('weatherId1').src = weatherIcon1Url;
+
+            document.getElementById('oneDayTemp').innerHTML = data.daily[0].temp.max + " °F";
+            document.getElementById('oneDayWind').innerHTML = data.daily[0].wind_speed + " MPH";
+            document.getElementById('oneDayHumidity').innerHTML = data.daily[0].humidity + " %";
+            //Day 2 Info
+            var weatherIcon2 = data.daily[1].weather[0].icon;
+            var weatherIcon2Url = "http://openweathermap.org/img/wn/" + weatherIcon2 + "@2x.png";
+            console.log(weatherIcon2);
+            document.getElementById('weatherId2').src = weatherIcon2Url;
+
+            document.getElementById('twoDayTemp').innerHTML = data.daily[1].temp.max + " °F";
+            document.getElementById('twoDayWind').innerHTML = data.daily[1].wind_speed + " MPH";
+            document.getElementById('twoDayHumidity').innerHTML = data.daily[1].humidity + " %";
+            //Day 3 Info
+            var weatherIcon3 = data.daily[2].weather[0].icon;
+            var weatherIcon3Url = "http://openweathermap.org/img/wn/" + weatherIcon3 +"@2x.png";
+            console.log(weatherIcon3);
+            document.getElementById('weatherId3').src = weatherIcon3Url;
+
+            document.getElementById('threeDayTemp').innerHTML = data.daily[2].temp.max + " °F";
+            document.getElementById('threeDayWind').innerHTML = data.daily[2].wind_speed + " MPH";
+            document.getElementById('threeDayHumidity').innerHTML = data.daily[2].humidity + " %";
+            //Day 4 Info
+            var weatherIcon4 = data.daily[3].weather[0].icon;
+            var weatherIcon4Url = "http://openweathermap.org/img/wn/" + weatherIcon4 +"@2x.png";
+            console.log(weatherIcon4);
+            document.getElementById('weatherId4').src = weatherIcon4Url;
+
+            document.getElementById('weatherId3').src = weatherIcon3Url;
+            document.getElementById('fourDayTemp').innerHTML = data.daily[3].temp.max + " °F";
+            document.getElementById('fourDayWind').innerHTML = data.daily[3].wind_speed + " MPH";
+            document.getElementById('fourDayHumidity').innerHTML = data.daily[3].humidity + " %";
+            //Day 5 Info
+            var weatherIcon5 = data.daily[4].weather[0].icon;
+            var weatherIcon5Url = "http://openweathermap.org/img/wn/" + weatherIcon5 +"@2x.png";
+            console.log(weatherIcon5);
+            document.getElementById('weatherId5').src = weatherIcon5Url;
+
+            document.getElementById('fiveDayTemp').innerHTML = data.daily[4].temp.max + " °F";
+            document.getElementById('fiveDayWind').innerHTML = data.daily[4].wind_speed + " MPH";
+            document.getElementById('fiveDayHumidity').innerHTML = data.daily[4].humidity + " %";
+        })
+    });
+
+
+})
 
 fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
     .then(function(response){
@@ -99,3 +160,7 @@ fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${lon
             document.getElementById('fiveDayHumidity').innerHTML = data.daily[4].humidity + " %";
         })
     });
+        })
+    })
+
+    
